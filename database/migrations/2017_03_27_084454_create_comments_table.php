e76384d3b976eb814609270d91e77d817f74708b
Julien BELMONT <jbelmont@groupe-ara.fr>
2017-03-27 11:38:51 +0200

    1er commit : migrations

---
+++ database/migrations/2017_03_27_084454_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('comments', function (Blueprint $table) {
+           $table->increments('id');
+           $table->integer('article_id')->unsigned();
+           $table->text('comment');
+           $table->string('author_email');
+           $table->string('author_name');
+           $table->boolean('visible');
+           $table->timestamps();
+
+           $table->foreign('article_id')->references('id')->on('articles');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
